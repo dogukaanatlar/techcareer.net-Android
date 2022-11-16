c06fd2e8ca5423b3932ffa009c5493fdfe168de7
Dogukaan Atlar <dogukaanatlar@gmail.com>
2022-11-16 23:11:01 +0300

    Odev1 eklendi

---
+++ Odev1/src/Main.java
@@ -0,0 +1,52 @@
+public class Main {
+    public static void main(String[] args) {
+        int postaKodu = 06111;
+        int stokMiktari = 98;
+        int siparisAdedi = 5;
+        int yayinlanmaTarihi = 1953;
+        int indirimMiktari = 50;
+        int odaSayisi = 45;
+        int agirlik = 209;
+        int otobusHatti = 121;
+        int kalanDakika = 5;
+        int takipKodu = 123123;
+        int kuponSuresi = 20;
+
+        double bakiye = 199.9;
+        double maas = 10900.5;
+        double yemekFiyati = 86.4;
+        double urunPuani = 6.7;
+        double ekranBoyutu = 6.8;
+
+        String sehir = "Ankara";
+        String ulke = "Turkiye";
+        String telefon = "01231231212";
+        String email = "deneme@deneme.com";
+        String meslek = "Muhendis";
+        String musteriAdi = "Dogukaan";
+        String dogumGunu = "01012022";
+        String medeniDurum = "Bekar";
+        String urunYorum = "Guzel urun";
+        String odemeTarihi = "01012022";
+        String odeme = "Nakit";
+        String arabaModeli = "Chevrolet-Captiva";
+        String kitapAdi = "Childhood's End";
+        String enlem = "39.57K";
+        String boylam = "32.53D";
+        String urunAdi = "Kalem";
+        String marka = "Apple";
+        String muzikAdi = "Riders on the Storm";
+        String videoSuresi = "03:09:37";
+        String resimAdi = "Cıglık";
+        String dosyaFormati = "png";
+        String renk = "kirmizi";
+        String renkKodu = "#c4151c";
+        String telefonModeli = "S22";
+        String ulusalGun = "29101923";
+        String tatilGunu = "01012022";
+        String rezervasyonTarihi = "01012022";
+        String sokakAdi = "Cumhuriyet";
+        String kuponKodu = "UCUZ20";
+        String faturaAdresi="Atatürk Mah. Cumhuriyet Cad.";
+    }
+}
